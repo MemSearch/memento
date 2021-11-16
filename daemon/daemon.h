10c1819ee9f9f66cc5a6e787df5de72fad420ad7
ezuryy <daria.kidinova@gmail.com>
2021-11-16 21:06:08 +0300

    initial commit

---
+++ daemon/daemon.h
@@ -0,0 +1,35 @@
+#ifndef PROJECT_DAEMON_H
+#define PROJECT_DAEMON_H
+
+#include <iostream>
+#include "sqlite3.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+class Daemon {
+public:
+    Daemon();
+    Daemon(const string& parsed, const string& recognized);
+    ~Daemon() {
+        sqlite3_close(parsedDB);
+        sqlite3_close(recognizedDB);
+    }
+    void work();
+    static string getURL();
+    void recognize();
+    static int getTemplateOfPicture(const string& url);
+    static string getTextInPicture(const string& url);
+    void writeRecognitionResults(const string& url, const int& pitTemplate, const string& text);
+
+private:
+    void initialize(const string& parsed, const string& recognized);
+
+    sqlite3* parsedDB;
+    sqlite3* recognizedDB;
+};
+
+int callbackEmpty(void* sum, int args, char** argv, char** azColName);
+
+#endif //PROJECT_DAEMON_H
