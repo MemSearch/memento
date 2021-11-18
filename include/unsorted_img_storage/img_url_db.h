e3a93f37bb4b93e7bba6dfea01bbe8ad0ac1ef8e
papazloynt <cool.chastikov53@gmail.com>
2021-11-18 05:39:11 +0300

    Add DB of img URL

---
+++ include/unsorted_img_storage/img_url_db.h
@@ -0,0 +1,44 @@
+#ifndef C_C_IMG_URL_DB_H
+#define C_C_IMG_URL_DB_H
+
+// C++ headers
+#include <iostream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <shared_mutex>
+
+// SQLite3
+#include <sqlite3.h>
+
+enum Status {
+    OK,
+    ERROR
+};
+
+class ImgUrlDB {
+public:
+    // Initialize repo
+    ImgUrlDB() : db(nullptr), err(nullptr) {
+        sqlite3_open("DataBase.db", &db);
+        if (CreateDataBase() == Status::ERROR) {
+            std::cerr << "Table wasn't created" << std::endl;
+        }
+    }
+
+    virtual ~ImgUrlDB() {
+        sqlite3_close(db);
+    }
+
+    Status InsertImg(const std::string imgUrl) { return Status::ERROR; }
+    std::vector<std::string> GetImgs() { return std::vector<std::string>();}
+
+private:
+    Status CreateDataBase();
+
+    sqlite3* db;
+    char* err;
+    std::shared_mutex mutex_;
+};
+
+#endif //C_C_IMG_URL_DB_H
