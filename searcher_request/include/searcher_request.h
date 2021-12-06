588c9f23d152261eef8fa2274072f01f4c02a646
HvarZ <hvarzahar@gmail.com>
2021-12-07 02:47:22 +0300

    add checking for forbidden symbols

--- searcher_request/include/searcher_request.h
+++ searcher_request/include/searcher_request.h
@@ -7,30 +7,34 @@
 
 #include "trie.h"
 
-using wordPopularity = std::pair<std::wstring, size_t>;
-
-struct Compare {
-  auto operator()(const wordPopularity &lhs,
-                  const wordPopularity &rhs) const noexcept -> bool;
-};
 
 class SearcherRequest final {
 public:
   SearcherRequest() = default;
   explicit SearcherRequest(const std::wstring& request);
-  explicit SearcherRequest(std::wstring&& request);
 
   void setRequest(const std::wstring& request);
-  void setRequest(std::wstring&& request);
+
+  [[nodiscard]] auto getResult() const noexcept -> std::vector<std::wstring>;
 
 private:
   std::wstring request_;
-  Trie trie;
+  Trie trie_;
+  static constexpr std::array<wchar_t, 28> forbiddenSymbols{
+      L',', L';', L'?', L'!',
+      L':', L'"', L'|', L'\\',
+      L'.', L'/',L'(', L')',
+      L'@', L'#', L'$', L'%',
+      L'^', L'&',  L'*', L'1',
+      L'2', L'3', L'4', L'5',
+      L'6', L'7', L'8', L'9'};
 
 private:
-  auto getPopularityWords() const noexcept -> std::set<wordPopularity>;
+  [[nodiscard]] auto getClusterWords() const noexcept ->
+      std::set<std::wstring>;
   [[nodiscard]] auto getSimilarWords() const noexcept ->
-      std::set<std::wstring, Compare>;
+      std::set<std::wstring>;
+  friend auto fixString(const std::wstring& string) -> std::wstring;
 };
 
 #endif // MEMENTO_SEARCHER_REQUEST_H
--- searcher_request/sources/searcher_request.cpp
+++ searcher_request/sources/searcher_request.cpp
@@ -1,3 +1,21 @@
 #include "searcher_request.h"
 #include "trie.h"
 
+auto fixString(const std::wstring &string) -> std::wstring {
+  std::wstring result = string;
+  for (auto& symbol : string) {
+    for (const auto& banSymbol : SearcherRequest::forbiddenSymbols) {
+      if (symbol == banSymbol) {
+        result.erase(symbol);
+        break;
+      }
+    }
+  }
+
+  return result;
+}
+
+SearcherRequest::SearcherRequest(const std::wstring &request) {
+  request_ = fixString(request);
+  auto clusterWords = get
+}
