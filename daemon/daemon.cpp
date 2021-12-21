9614614aa0fa04546f8ca28821d9a53cf86ba285
ezuryy <daria.kidinova@gmail.com>
2021-12-21 19:36:56 +0300

    change pointer to db to unique_ptr

--- daemon/daemon.cpp
+++ daemon/daemon.cpp
@@ -1,12 +1,12 @@
 #include "daemon.h"
+#include <opencv2/opencv.hpp>
 
-Daemon::Daemon(const string& connInfo) {
-    conn = PQconnectdb(connInfo.c_str());
+Daemon::Daemon(const std::string& connInfo) : conn(PQconnectdb(connInfo.c_str()), PQfinish) {
 
-    if (PQstatus(conn) != CONNECTION_OK) {
-        std::cout << "Connection to database failed: " << PQerrorMessage(conn) << "\n";
-        PQfinish(conn);
-        // TODO: throw exception
+    if (PQstatus(conn.get()) != CONNECTION_OK) {
+        std::cout << "Connection to database failed: " << PQerrorMessage(conn.get()) << "\n";
+        PQfinish(conn.get());
+        throw std::invalid_argument("Connection to database failed");
     } else {
         std::cout << "Connection to database succeed." << std::endl;
     }
@@ -17,8 +17,8 @@ static void signalHandler(int signum) {
     exit(signum);
 }
 
-std::pair<int, string> Daemon::getPath() {
-    PGresult *res = PQexec(conn, "SELECT id, name FROM new_table ORDER BY id DESC limit 1;");
+std::pair<int, std::string> Daemon::getPath() {
+    PGresult *res = PQexec(conn.get(), "SELECT id, name FROM new_table ORDER BY id DESC limit 1;");
     if (PQresultStatus(res) != PGRES_TUPLES_OK) {
         std::cout << "Select failed: " << PQresultErrorMessage(res) << std::endl;
     } else {
@@ -29,8 +29,8 @@ std::pair<int, string> Daemon::getPath() {
                   << PQgetvalue(res, 0, 1) << '\n';
         char buffer[100];
         strncpy(buffer, PQgetvalue(res, 0, 0), 100);
-        int id = std::stoi(string(buffer));
-        std::pair<int, string> result = {id, string(PQgetvalue(res, 0, 1))};
+        int id = std::stoi(std::string(buffer));
+        std::pair<int, std::string> result = {id, std::string(PQgetvalue(res, 0, 1))};
         PQclear(res);
         return result;
     }
@@ -38,10 +38,10 @@ std::pair<int, string> Daemon::getPath() {
     return {0, ""};
 }
 
-void Daemon::removeRecord(const std::pair<int, string>& record) {
-    const string command =
+void Daemon::removeRecord(const std::pair<int, std::string>& record) {
+    const std::string command =
             "DELETE FROM new_table WHERE id = " + std::to_string(record.first) + " AND name = '" + record.second + "'";
-    PGresult *res = PQexec(conn, command.c_str());
+    PGresult *res = PQexec(conn.get(), command.c_str());
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
         std::cout << "Delete from table failed: " << PQresultErrorMessage(res)
                   << std::endl;
@@ -52,22 +52,22 @@ void Daemon::removeRecord(const std::pair<int, string>& record) {
 int Daemon::getBiggestID() {
     int result = 0;
 
-    PGresult *res = PQexec(conn, "SELECT id FROM new_table ORDER BY id DESC limit 1;");
+    PGresult *res = PQexec(conn.get(), "SELECT id FROM new_table ORDER BY id DESC limit 1;");
     if (PQresultStatus(res) != PGRES_TUPLES_OK) {
         std::cout << "Select failed: " << PQresultErrorMessage(res) << std::endl;
     } else if (PQntuples(res) != 0) {
         char buffer[100];
         strncpy(buffer, PQgetvalue(res, 0, 0), 100);
-        result = std::stoi(string(buffer));
+        result = std::stoi(std::string(buffer));
     }
     PQclear(res);
     return result;
 }
 
-void Daemon::insertRecord(int id, const string& path, int pattern , const string& text) {
-    const string command =
+void Daemon::insertRecord(int id, const std::string& path, int pattern , const std::string& text) {
+    const std::string command =
             "INSERT INTO parsed VALUES (" + std::to_string(id) + ", '" + path + "', " + std::to_string(pattern) + ", '" + text + "');";
-    PGresult *res = PQexec(conn, command.c_str());
+    PGresult *res = PQexec(conn.get(), command.c_str());
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
         std::cout << "Insert into table failed: " << PQresultErrorMessage(res)
                   << std::endl;
@@ -78,7 +78,7 @@ void Daemon::insertRecord(int id, const string& path, int pattern , const string
 bool Daemon::isEmpty() {
     bool empty = true;
 
-    PGresult *res = PQexec(conn, "SELECT * FROM new_table ORDER BY id DESC limit 1;");
+    PGresult *res = PQexec(conn.get(), "SELECT * FROM new_table ORDER BY id DESC limit 1;");
     if (PQresultStatus(res) != PGRES_TUPLES_OK) {
         std::cout << "Select failed: " << PQresultErrorMessage(res) << std::endl;
     } else if (PQntuples(res) != 0) {
@@ -106,18 +106,18 @@ void Daemon::recognize() {
         }
         int id = getBiggestID() + 1;
         int picTemplate = getTemplateOfPicture(record.second);
-        string picText = getTextInPicture(record.second);
+        std::string picText = getTextInPicture(record.second);
         insertRecord(id, record.second, picTemplate, picText);
     }
 }
 
-int Daemon::getTemplateOfPicture(const string& path) {
+int Daemon::getTemplateOfPicture(const std::string& path) {
     /// TODO: определяем шаблон картинки
     return 1;
 }
 
-string Daemon::getTextInPicture(const string& path) {
-    string text;
+std::string Daemon::getTextInPicture(const std::string& path) {
+    std::string text;
     auto *ocr = new tesseract::TessBaseAPI();
 
     // Initialize OCR engine to use English (eng) and The LSTM OCR engine.
@@ -127,12 +127,12 @@ string Daemon::getTextInPicture(const string& path) {
     ocr->SetPageSegMode(tesseract::PSM_AUTO);
 
     // Open input image using OpenCV
-    Mat im = cv::imread(path, IMREAD_COLOR);
+    cv::Mat im = cv::imread(path, cv::IMREAD_COLOR);
 
     // Set image data
     ocr->SetImage(im.data, im.cols, im.rows, 3, static_cast<int>(im.step));
 
-    text = string(ocr->GetUTF8Text());
+    text = std::string(ocr->GetUTF8Text());
 
     ocr->End();
     return text;
--- daemon/daemon.h
+++ daemon/daemon.h
@@ -4,35 +4,32 @@
 #include <iostream>
 #include <libpq-fe.h>
 #include <tesseract/baseapi.h>
-#include <opencv2/opencv.hpp>
 #include <cstdlib>
 #include <string>
 #include <thread>
 #include <chrono>
 #include <csignal>
-
-using std::string;
-using namespace cv;
+//#include <memory>
 
 class Daemon {
 public:
-    explicit Daemon(const string& connInfo);
+    explicit Daemon(const std::string& connInfo);
     Daemon() = delete;
     ~Daemon() {
-        PQfinish(conn);
+//        PQfinish(conn.get());
     }
-    std::pair<int, string> getPath();
-    void removeRecord(const std::pair<int, string>& record);
+    std::pair<int, std::string> getPath();
+    void removeRecord(const std::pair<int, std::string>& record);
     int getBiggestID();
-    void insertRecord(int id, const string& path, int pattern , const string& text);
+    void insertRecord(int id, const std::string& path, int pattern , const std::string& text);
     bool isEmpty();
     void recognize();
 
 private:
-    int getTemplateOfPicture(const string& path);
-    string getTextInPicture(const string& path);
+    int getTemplateOfPicture(const std::string& path);
+    std::string getTextInPicture(const std::string& path);
 
-    PGconn *conn;
+    std::unique_ptr<PGconn, void(*)(PGconn *)> conn;
 };
 
 #endif //PROJECT_DAEMON_H
