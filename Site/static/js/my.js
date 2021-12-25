67cb6dbd7be93a9159e78ddc12b7377acbe4acf8
ezuryy <daria.kidinova@gmail.com>
2021-12-25 10:02:49 +0300

    some changes in site

--- Site/static/js/my.js
+++ Site/static/js/my.js
@@ -1,5 +1,51 @@
 "use strict"
 
+function downloadAllChecked() {
+    var meme_templates = document.getElementById("mem-info").querySelectorAll(".meme-template");
+    const urls = [];
+    for (let index = 0; index < meme_templates.length; index++) {
+        if (meme_templates[index].checked) {
+            var pic = meme_templates[index].parentElement.querySelectorAll(".mem-pic");
+            var pic_src = pic[0].src;
+            // var pic_webp = pic_src.substr(0, pic_src.lastIndexOf('.')) + ".JPEG";
+            // console.log(pic_webp);
+            // console.log(pic[0].src);
+            urls.push(pic_src);
+        }
+    }
+    console.log(urls);
+    downloadAll(urls);
+};
+
+function downloadAll(urls) {
+    var link = document.createElement('a');
+
+    link.setAttribute('download', null);
+    link.style.display = 'none';
+
+    document.body.appendChild(link);
+
+    for (var i = 0; i < urls.length; i++) {
+        link.setAttribute('href', urls[i]);
+        link.click();
+    }
+
+    document.body.removeChild(link);
+}
+
+function selectAllCheckBox() {
+    var meme_templates = document.getElementById("mem-info").querySelectorAll(".meme-template");
+    selectAll(meme_templates);
+};
+
+function selectAll(some_class) {
+    console.log(some_class);
+    for (let index = 0; index < some_class.length; index++) {
+        if (!some_class[index].checked) {
+            some_class[index].checked = true;
+        }
+    }
+};
 
 function viewDiv() {
     document.getElementById("mem-search").style.display = "block";
@@ -46,37 +92,17 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         if (error === 0) {
+            console.log(document.querySelectorAll(".center"));
             if (document.getElementById("tags-error")) {
                 var err_txt = document.getElementById("tags-error");
                 err_txt.parentElement.removeChild(err_txt);
             }
             openMemInfo();
 
-            const radios = document.querySelectorAll('input[name="templ"]')
-            var radTempl;
-            for (const r of radios) {
-                if(r.checked) {
-                    radTempl = r.value;
-                    break;
-                }
-            }
             var txtData = document.getElementById("tags-text").value;
             console.log(txtData);
 
-            // var data = {
-            //     url1: "../static/img/Superman.jpg",
-            //     url2: "../static/img/thanus.jpg",
-            //     url3: "../static/img/thanus.jpg",
-            //     url4: "../static/img/Superman.jpg",
-            //     url5: "../static/img/Superman.jpg",
-            //     url6: "../static/img/thanus.jpg",
-            //     url7: "../static/img/thanus.jpg",
-            //     url8: "../static/img/Superman.jpg",
-            //     url9: "../static/img/Superman.jpg",
-            // };
-
             var data = {
-                template: radTempl,
                 text: txtData,
             };
 
@@ -101,17 +127,19 @@ document.addEventListener('DOMContentLoaded', function () {
             var div = document.createElement('div');
             div.id = "mem-pics";
             document.getElementById('mem-info').appendChild(div);
+            var mem_btn_div = document.getElementById('mem-btn');
             console.log(urls);
-
+            
             if (urls === null) {
+                // mem_btn_div.style.display = "none";
                 div.innerHTML = "<h4 class=center>Sorry, we couldn't find anything for your query.</h4>";
-                div.innerHTML += "<h5 class=center>You can repeat the request or try to find the meme you are interested in yourself.</h5>";
+                div.innerHTML += "<h5 class=center>You can repeat the request or try to find the meme you are interested in yourself.</h5>"; 
 
                 var div_in = document.createElement('div');
                 div_in.classList = "center";
 
                 div_in.innerHTML += "<h5>You can also help us and send a meme that you couldn't find with us</h5>";
-
+                
                 var label = "<label for=file-upload class=custom-file-upload accept='.jpg, .jpeg, .png'>upload</label>"
                 div_in.innerHTML += label;
 
@@ -129,14 +157,14 @@ document.addEventListener('DOMContentLoaded', function () {
                 div.appendChild(div_in);
             }
             else {
-                var urls_count = Object.keys(urls).length;
-                var url_cnt = 0;
                 div.innerHTML = "<h3 class=center>Memes that we were able to find:</h3>";
+                var urls_count = Object.keys(urls).length
+                var url_cnt = 0;
                 var div_in = document.createElement('div');
                 div_in.classList = "center";
-
                 for (var url in urls) {
-                    div_in.innerHTML += "<img id=" + url + " class=mem-pic src=" + urls[url] + ' alt="">'
+                    div_in.innerHTML += "<label><input type='checkbox' class='meme-template' name='templ'>\
+                    <img id=" + url + " class=mem-pic src=" + urls[url] + "></label>";
                     if (url_cnt % 2 === 1) {
                         document.getElementById('mem-pics').appendChild(div_in);
                         div_in = document.createElement('div');
@@ -147,37 +175,36 @@ document.addEventListener('DOMContentLoaded', function () {
                         document.getElementById('mem-pics').appendChild(div_in);
                     }
                 }
+                // mem_btn_div.style.display = "flex";
+                var div_upload = document.createElement('div');
+                div_upload.classList = "center up-margin";
+                // var select_all_btn = document.createElement('button');
+                // select_all_btn.textContent = "select all";
+                // select_all_btn.classList = "search-button left-margin";
+                // select_all_btn.onclick = function() {
+                //     alert("SAS");
+                // }
+                // div_upload.appendChild(select_all_btn);
+                // <button class="search-button left-margin" onclick="selectAllCheckBox()">select all</button>\
+                div_upload.innerHTML += '<button class="search-button left-margin" onclick="selectAllCheckBox()">select all</button>\
+                                         <h3 class="">Ready to upload?</h3>\
+                                         <button class="search-button" onclick="downloadAllChecked()">upload</button>';
+                // <a href="../static/img/thanus.jpg" download="">\</a>
+                document.getElementById('mem-pics').appendChild(div_upload);
             }
         }
-    }
 
+    }
     function formAddError(input) {
         input.parentElement.classList.add("_error");
         input.classList.add("_error");
     }
-
+    
     function formRemoveError(input) {
         input.parentElement.classList.remove("._error");
         input.classList.remove("_error");
     }
-
 });
-
-// const sendInfo = () => {
-//     const form = document.querySelector(".form_body")
-
-//     form.addEventListener('submit', e => {
-//         const formData = new FormData(form)
-//         sendData("https://jsonplaceholder.typicode.com/posts", formData)
-//             .then(() => {
-//                 form.reset();
-//             })
-//             .catch((err) => {
-//                 console.log(err);
-//             })
-//     });
-// }
-
 
 const getData = async (url) => {
     const response = await fetch(url);
@@ -186,7 +213,7 @@ const getData = async (url) => {
     }
     return await response.json();
 };
-
+    
 const sendData = (url, data) => {
     const response = fetch(url, {
         method: 'GET',
